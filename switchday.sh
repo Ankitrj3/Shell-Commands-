78ae77f808364f4ed1baaac45e7ed2643f0d6f71
Ankit Ranjan <110593132+Ankitrj3@users.noreply.github.com>
2024-02-13 09:30:46 +0530

    Add files via upload

---
+++ switchday.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "enter the number";
+read a;
+
+case $a in pattern);;
+
+    1) echo "monday";;
+    
+    2) echo "tuesday";;
+    3) echo "wednesday";;
+    4) echo "thrusday";;
+    5) echo "friday";;
+    *) echo "no working day";;
+esac
+   
